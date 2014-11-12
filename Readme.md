e7acd21d67b0876906c660af2608e43d7fb9f3bc
Joel Kemp <mrjoelkemp@gmail.com>
2014-11-12 14:48:26 -0500

    ES6 support. Fixes #3

--- Readme.md
+++ Readme.md
@@ -1,17 +1,18 @@
-### Precinct
+### Precinct [![npm](http://img.shields.io/npm/v/module-definition.svg)](https://npmjs.org/package/precinct) [![npm](http://img.shields.io/npm/dm/precinct.svg)](https://npmjs.org/package/precinct)
 
 > Unleash the detectives
 
 `npm install precinct`
 
-Uses the appropriate detective to find the dependencies of the file. Supports: AMD and CommonJS modules.
+Uses the appropriate detective to find the dependencies of the file. 
+Supports: AMD, CommonJS, and ES6 modules.
 
 ### Usage
 
 ```js
 var precinct = require('precinct');
 
-var deps = precinct('myFile.js');
+var deps = precinct('myFile.js'); // array containing dependencies
 ```
 
 ### License
--- test/index.js
+++ test/index.js
@@ -1,17 +1,38 @@
 var precinct = require('../'),
     assert = require('assert');
 
+describe('node-precinct', function() {
+  it('grabs dependencies of amd modules', function () {
+    var amd  = precinct(__dirname + '/amd.js');
+    assert(amd.indexOf('./a') !== -1);
+    assert(amd.indexOf('./b') !== -1);
+    assert(amd.length === 2);
+  });
 
-var amd  = precinct(__dirname + '/amd.js');
-var cjs  = precinct(__dirname + '/commonjs.js');
-var none = precinct(__dirname + '/none.js');
+  it('grabs dependencies of commonjs modules', function () {
+    var cjs  = precinct(__dirname + '/commonjs.js');
+    assert(cjs.indexOf('./a') !== -1);
+    assert(cjs.indexOf('./b') !== -1);
+    assert(cjs.length === 2);
+  });
 
-assert(amd.indexOf('./a') !== -1);
-assert(amd.indexOf('./b') !== -1);
-assert(amd.length === 2);
+  it('grabs dependencies of es6 modules', function () {
+    var cjs  = precinct(__dirname + '/es6.js');
+    assert(cjs.indexOf('lib') !== -1);
+    assert(cjs.length === 1);
+  });
 
-assert(cjs.indexOf('./a') !== -1);
-assert(cjs.indexOf('./b') !== -1);
-assert(cjs.length === 2);
+  it('yields no dependencies for es6 modules with no imports', function () {
+    var cjs = precinct(__dirname + '/es6NoImport.js');
+    assert(!cjs.length);
+  });
 
-assert(none.length === 0);
+  it('yields no dependencies for non-modules', function () {
+    var none = precinct(__dirname + '/none.js');
+    assert(!none.length);
+  });
+});
+
+
+
+
