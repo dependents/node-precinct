efc41de236fb6c8ed5f760d4489a679fe9cc6c71
Joel Kemp <jokemp@adobe.com>
2014-11-17 16:05:18 -0500

    SASS support. Fixes #1

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var getModuleType = require('module-definition'),
-    detective = require('detective'),
-    detectiveAmd = require('detective-amd'),
-    detectiveEs6 = require('detective-es6'),
-    fs = require('fs');
+var getModuleType = require('module-definition');
+var detective = require('detective');
+var detectiveAmd = require('detective-amd');
+var detectiveEs6 = require('detective-es6');
+var detectiveSass = require('detective-sass');
+var fs = require('fs');
 
 /**
  * Finds the list of dependencies for the given file
- * @param  {String} file - path of the file whose dependencies to find
+ * @param {String} file - path of the file whose dependencies to find
+ * @param {String} [type] - The type of content being passed in. Useful if you want to use a non-js detective
  * @return {String[]}
  */
-module.exports = function(content) {
-  var type = getModuleType.fromSource(content),
-      theDetective,
-      dependencies = [];
+module.exports = function(content, type) {
+  var dependencies = [];
+  var theDetective;
 
-  switch(type) {
+  type = type || getModuleType.fromSource(content);
+
+  switch (type) {
     case 'commonjs':
       theDetective = detective;
       break;
@@ -23,6 +26,10 @@ module.exports = function(content) {
       break;
     case 'es6':
       theDetective = detectiveEs6;
+      break;
+    case 'sass':
+      theDetective = detectiveSass;
+      break;
   }
 
   if (theDetective) {
