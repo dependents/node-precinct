3ce6d791f9b3c10a140537a7da25933b16efc3f6
Joel Kemp <jokemp@adobe.com>
2014-11-24 10:57:48 -0500

    JSCS for stylechecking

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 /**
  * Finds the list of dependencies for the given file
- * @param {String} file - path of the file whose dependencies to find
+ * @param {String} content - path of the file whose dependencies to find
  * @param {String} [type] - The type of content being passed in. Useful if you want to use a non-js detective
  * @return {String[]}
  */
