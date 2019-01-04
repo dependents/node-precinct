7955a7c7b922fb3f76e8ff3e97bdcfea7a1e0a55
Ben Scott <ben.scott@shopify.com>
2019-01-04 15:38:14 -0800

    Add --type CLI argument

--- bin/cli.js
+++ bin/cli.js
@@ -10,6 +10,7 @@ program
   .version(require('../package.json').version)
   .usage('[options] <filename>')
   .option('--es6-mixedImports')
+  .option('-t, --type <type>', 'The type of content being passed in. Useful if you want to use a non-js detective')
   .parse(process.argv);
 
 var content = fs.readFileSync(program.args[0], 'utf8');
@@ -20,6 +21,10 @@ var options = {
 
 if (program['es6MixedImports']) {
   options.es6.mixedImports = true;
+}
+
+if (program.type) {
+  options.type = program.type;
 }
 
 console.log(precinct(content, options));
