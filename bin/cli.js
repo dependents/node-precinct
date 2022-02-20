c10bfe7bd755f84fe87555a62b52107a342c78a8
XhmikosR <xhmikosr@gmail.com>
2022-02-20 15:51:19 +0200

    Improve CLI

--- bin/cli.js
+++ bin/cli.js
@@ -8,26 +8,20 @@ const precinct = require('../index.js');
 const { version } = require('../package.json');
 
 program
+  .arguments('<filename>')
   .version(version)
-  .usage('[options] <filename>')
-  .option('--es6-mixedImports')
-  .option('-t, --type <type>', 'The type of content being passed in. Useful if you want to use a non-js detective')
-  .parse(process.argv);
-
-const content = fs.readFileSync(program.args[0], 'utf8');
-
-const options = {
-  es6: {}
-};
+  .option('--es6-mixedImports', 'Fetch all dependendies from a file that contains both CJS and ES6 imports')
+  .option('-t, --type <type>', 'The type of content being passed in. Useful if you want to use a non-JS detective')
+  .parse();
 
 const cliOptions = program.opts();
+const options = {
+  es6: {
+    mixedImports: cliOptions.es6MixedImports
+  },
+  type: cliOptions.type
+};
 
-if (cliOptions.es6MixedImports) {
-  options.es6.mixedImports = true;
-}
-
-if (cliOptions.type) {
-  options.type = cliOptions.type;
-}
+const content = fs.readFileSync(program.args[0], 'utf8');
 
 console.log(precinct(content, options).join('\n'));
